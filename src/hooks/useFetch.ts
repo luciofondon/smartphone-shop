705c2390f2a8bd46112373c4695a99b44d7a3add
Lucio David Fondón Terrón <lucio.fondonterron@plexus.es>
2022-10-27 22:20:04 +0200

    Fix useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,32 +1,38 @@
 import { useEffect, useRef, useState } from 'react';
 import { getHoursDiff } from '../utils/date-util';
 
+const EXPIRATION_HOUR = Number(process.env.REACT_APP_EXPIRATION_CACHE_HOUR);
+
 const useFetch = <S>(url: string, key: string, initialState: S): { data: S } => {
   const [data, setData] = useState<S>(initialState);
 
   useEffect(() => {
-    (async () => {
-      const data = localStorage.getItem(key);
-      if (data) {
-        const dataLocalStorage = JSON.parse(data);
-        const date = new Date(dataLocalStorage.date);
-        console.log(date);
-        if (getHoursDiff(date, new Date()) > 2) {
-          localStorage.removeItem(key);
-        }
+    const fetchCacheData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      localStorage.setItem(
+        key,
+        JSON.stringify({
+          date: new Date(),
+          data,
+        }),
+      );
+      setData(data);
+    }
 
-        setData(dataLocalStorage.data);
+    (async () => {
+      const result = localStorage.getItem(key);
+      if (result) {
+        const dataLocalStorage = JSON.parse(result);
+        const date = new Date(dataLocalStorage.date);
+        if (getHoursDiff(date, new Date()) > EXPIRATION_HOUR) {
+          localStorage.removeItem(key);
+          await fetchCacheData();
+        } else {
+          setData(dataLocalStorage.data);
+        }
       } else {
-        const response = await fetch(url);
-        const data = await response.json();
-        localStorage.setItem(
-          key,
-          JSON.stringify({
-            date: new Date(),
-            data,
-          }),
-        );
-        setData(data);
+        await fetchCacheData();
       }
     })();
   }, [url]);
